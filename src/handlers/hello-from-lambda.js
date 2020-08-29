20b9bc81945ccdd332915540a7b0928ca35570c2
Douglas Alves Domingues <kamikai@gmail.com>
2020-08-29 19:32:10 +0000

    use axios to make get request

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -1,13 +1,14 @@
+const axios = require('axios');
 /**
  * A Lambda function that returns a string.
  */
 exports.helloFromLambdaHandler = async () => {
     // If you change this message, you will need to adjust tests in hello-from-lambda.test.js
     const message = 'Hello from Lambda!';
-
+    const response = await axios.get('http://google.com').catch(error => error);
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log(message);
 
-    return message;
+    return response.data;
 };
